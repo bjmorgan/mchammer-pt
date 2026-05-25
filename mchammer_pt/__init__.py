85fda87d284e9d855cd2add1a800bc2b59d05266
Benjamin <b.j.morgan@bath.ac.uk>
2026-05-25 06:24:39 +0100

    release: 0.14.0

--- mchammer_pt/__init__.py
+++ mchammer_pt/__init__.py
@@ -32,7 +32,7 @@ from .wl import WangLandauParallelTempering
 from .wl_merge_diagnostics import MergeEvent
 from .wl_replica import WangLandauReplica
 
-__version__ = "0.13.0"
+__version__ = "0.14.0"
 
 __all__ = [
     "BaseParallelTempering",
